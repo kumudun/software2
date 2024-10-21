2cd35e20c767d2936cad346f90e86c3089364375
kumudun <kumudun@metropolia.fi>
2024-10-22 01:59:46 +0300

    Initial commit

---
+++ car.py
@@ -0,0 +1,6 @@
+class Car:
+    def __init__(self, reg_num, max_speed, cur_speed = 0, travel_dist = 0):
+        self.registration_number = reg_num
+        self.maximum_speed = max_speed
+        self.current_speed = cur_speed
+        self.travelled_distance = travel_dist
