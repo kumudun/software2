1ae00f8c05b1034e6669f7d9868e648ef22563ec
kumudun <kumudun@metropolia.fi>
2024-10-27 04:01:02 +0300

    changes new

--- exercise9.py
+++ exercise9.py
@@ -1,35 +1,74 @@
 # Fundamentals of object-oriented programming
-
+import random
 # Question 1
 
-from car import Car
+class Car:
+    def __init__(self, regPlate, maxSpeed,):
+        
+        self.regPlate = regPlate
+        self.maxSpeed = maxSpeed
+        self.current_speed = 0
+        self.odometer = 0
 
-car1 = Car('ABC-123',142)
+    def accelerate(self,acceleration):
+        self.current_speed = min(max(self.current_speed + acceleration, 0), self.maxSpeed)
 
-print (f"""
-registration number: {car1.registration_number}
-maximum speed: {car1.maximum_speed}
-current speed: {car1.current_speed}
-travelled distance: {car1.travelled_distance}
-""")
+    def drive(self,time):
+        self.odometer += self.current_speed * time
+
+
+if __name__ == "__main__":
+    porsche =Car(regPlate = "ABC-123" , maxSpeed=142)
+
+    print(f"Register plate {porsche.regPlate}, Maximum speed {porsche.maxSpeed}km/h,"
+          f"Current speed {porsche.current_speed}km/h,Odometer {porsche.odometer}km")
 
 # Question 2
-
-car1.accelerate(30)
-car1.accelerate(70)
-car1.accelerate(50)
-print(f"current speed: {car1.current_speed}km/h")
-
-car1.accelerate(-200)
-print(f"current speed: {car1.current_speed}km/h")
+    porsche.accelerate(30)
+    porsche.accelerate(70)
+    porsche.accelerate(50)
+    print (f"Current speed is {porsche.current_speed}km/h")
+    porsche.accelerate(-200)
+    print(f"Current speed is {porsche.current_speed}km/h")
 
 # Question 3
-
-car1.accelerate(50)
-car1.drive(1.5)
-print(f"travelled distance: {car1.travelled_distance}km/h")
+    porsche.accelerate(60)
+    porsche.drive(1.5)
+    print(f"Distance travelled is {porsche.odometer}km")
 
 # Question 4
+    cars =[]
+    for i in range (10):
+        cars.append(Car("ABC-" + str(i+1), random.randint(100,200)))
+
+    distance_max =0.
+    while distance_max < 10000:
+        for car in cars:
+            car.accelerate(random.randint(-10,15))
+            car.drive(1.)
+            distance_max = max(car.odometer, distance_max)
+
+    for car in cars:
+        print(f"{car.regPlate},"
+              f"{car.maxSpeed} , {car.current_speed}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
